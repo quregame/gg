2f1d7562655ff579e8bf11d55ce36eaa8bdbd3d1
Студенческая Учетная запись факультета информатики <student_kash@study.rsvpu.ru>
2019-10-29 09:13:23 +0500

    PECW-3 Main menu added

---
+++ Assets/Scripts/menu_script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menu_script : MonoBehaviour
+{
+    public GameObject button_play;
+
+    public void NewGame()
+    {
+        Application.LoadLevel("Map");
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
