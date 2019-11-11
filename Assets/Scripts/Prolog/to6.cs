a2af000959a48b92a8865189475bbd325b5888c6
qurebeer <alexey.krivtsoff@yandex.ru>
2019-11-11 18:36:39 +0500

    prolog added

---
+++ Assets/Scripts/Prolog/to6.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class to6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            Application.LoadLevel("pr6");
+        }
+        if (Input.GetMouseButtonDown(1) == true)
+        {
+            Application.LoadLevel("pr6");
+        }
+    }
+}
