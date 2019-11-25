1528573ff7106e21558d9e2a85af11042eca6ff5
qurebeer <alexey.krivtsoff@yandex.ru>
2019-11-25 17:11:46 +0500

    togglesound

--- Assets/Scripts/menu_script.cs
+++ Assets/Scripts/menu_script.cs
@@ -5,17 +5,47 @@ using UnityEngine.UI;
 
 public class menu_script : MonoBehaviour
 {
+    public int sound;
+    public GameObject button_sound;
+    
+    [SerializeField]
+    Sprite sound_on;
+    [SerializeField]
+    Sprite sound_off;
+    
     public void NewGame()
     {
         Application.LoadLevel("pr1");
     }
+
+    public void ToggleSound()
+    {
+        if (sound == 1)
+        {
+            AudioListener.volume = 0;
+            sound = 0;
+        }
+        else if (sound == 0)
+        {
+            AudioListener.volume = 1;
+            sound = 1;
+        }
+    }
+
     void Start()
     {
-        
+        sound = 1;
     }
 
     void Update()
     {
-        
+        if (sound == 1)
+        {
+            button_sound.GetComponent<Image>().sprite = sound_on;
+        }
+        else if(sound == 0)
+        {
+            button_sound.GetComponent<Image>().sprite = sound_off;
+        }
     }
 }
