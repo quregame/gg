997fa1d5e5f6b3da2d13b9529ed6a2559542a237
qurebeer <alexey.krivtsoff@yandex.ru>
2019-12-20 13:06:20 +0500

    compiled

---
+++ Assets/Scripts/WindowStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowStats : MonoBehaviour
+{
+    public Button acc;
+
+    public void close()
+    {
+        gameObject.SetActive(false);
+    }
+}
